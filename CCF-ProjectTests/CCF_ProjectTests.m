f903a4b959bcc898f222d6233d544f4d8853222b
jovhengshuapps <jovhenni19@yahoo.com>
2017-02-13 05:09:39 +0800

    project files

---
+++ CCF-ProjectTests/CCF_ProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  CCF_ProjectTests.m
+//  CCF-ProjectTests
+//
+//  Created by Joshua Jose Pecson on 20/01/2017.
+//  Copyright © 2017 JoVhengshua Apps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CCF_ProjectTests : XCTestCase
+
+@end
+
+@implementation CCF_ProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
